ffee18d5115d52dc57a1fce3cf3d00bced85da44
Nerower <p0sirega121234@gmail.com>
2022-09-22 22:31:12 +0300

    l1: Create matrix multiplication function

--- l1/matrix.py
+++ l1/matrix.py
@@ -11,13 +11,52 @@ def transpont(m):
     return tm
 
 
-if __name__ == "main":
+def mult(a,b):
+    if (len(a[0]) == len(b)):
+        common_side = len(b)
+    else:
+        raise "Impossible to multiply matrixes"
+
+    res_rows = len(a)
+    res_cols = len(b[0])
+
+    res = []
+
+    for i in range(res_rows):
+        res.append([])
+
+        for j in range(res_cols):
+            sum = 0
+
+            for r in range(common_side):
+                sum += a[i][r] * b[r][j]
+
+            res[i].append(sum)
+
+    return res
+
+
+
+if __name__ == "__main__":
     m = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [0,0,0]
+        [ 6, 3, -1, 0],
+        [ 1, 1,  0, 4],
+        [-2, 5,  0, 2]
     ]
 
+    n = [
+        [2, 1, 4],
+        [0, 1, 1]
+    ]
+
+    # from pprint import pprint as pp
+
+    print("Transpont matrix m:")
+    # pp(transpont(m))
     for lst in transpont(m):
+        print(lst)
+
+    print("\n\nMultiply matrix n with m:")
+    # pp(mult(n,m))
+    for lst in mult(n,m):
         print(lst)
